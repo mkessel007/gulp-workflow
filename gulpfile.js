2ddd33d7551ad7ab2f7f1cb2f4512f8cb2df0614
Michael Kessel <mjkessel@gmail.com>
2014-08-29 11:08:58 -0700

    Added Browserfy, imported in jquery and mustache libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
+
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
 
 var jsSources = [
   'components/scripts/rclick.js',
@@ -13,15 +16,24 @@ var jsSources = [
 ];
 
 
+
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
     .pipe(coffee({ bare: true })
-    .on('error', gutil.log))
+      .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 });
+
+
+
+
+
+
+
